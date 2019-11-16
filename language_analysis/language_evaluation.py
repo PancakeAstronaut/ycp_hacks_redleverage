1e7432e691bda6dcfb9f548d17737562558e3e8b
PancakeAstronaut <senormrsir@gmail.com>
2019-11-15 21:12:58 -0500

    added a sentiment polarity analysis file, currently returns a string of tone

---
+++ language_analysis/language_evaluation.py
@@ -0,0 +1,32 @@
+
+
+def arb():
+    print("")
+
+
+def get_chat_tone(sentiment_polarity):
+
+    def tone_generator(chattone):
+        if chattone == 1:
+            tone = "Positive"
+        elif chat_tone == 2:
+            tone = "Negative"
+        else:
+            tone = "Average"
+
+        return tone
+
+    rounded_sent_pol = round(sentiment_polarity, 2)
+    if rounded_sent_pol >= .80:
+        chat_tone = 1
+    elif rounded_sent_pol <= .25:
+        chat_tone = 2
+    else:
+        chat_tone = 3
+
+    tone = tone_generator(chat_tone)
+    return tone
+
+
+if __name__ == '__main__':
+    arb()
