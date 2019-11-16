59a6fa6edd2545e44d6156532933c916e34826cf
PancakeAstronaut <senormrsir@gmail.com>
2019-11-15 20:29:07 -0500

    Added Global info and the sentiment analysis module.

---
+++ globals/API_INFO.py
@@ -0,0 +1,9 @@
+from azure.cognitiveservices.language.textanalytics import TextAnalyticsClient      # Azure API imports
+from msrest.authentication import CognitiveServicesCredentials
+
+
+# Azure API information for Text analysis
+azure_api_key = "d671436ef9ca42fb9b9205b8b209528c"
+credentials = CognitiveServicesCredentials(azure_api_key)
+azure_endpoint_url = "https://eastus.api.cognitive.microsoft.com/"
+azure_text_analytics = TextAnalyticsClient(endpoint=azure_endpoint_url, credentials=credentials)
---
+++ language_analysis/sentiment_analysis.py
@@ -0,0 +1,30 @@
+from globals import API_INFO
+
+sentiment_polarity = []
+
+
+def arb():
+    print("")
+
+
+def get_sentiment(input_text):
+    inputVar = input_text
+    documents = [  # Dictionary to hold basic values for the API to evaluate
+        {
+            "id": "1",
+            "language": "en",
+            "text": inputVar
+        }
+    ]
+
+    sent_polarity = API_INFO.azure_text_analytics.sentiment(documents=documents)  # adding a variable for sentiment polarity
+    for document in sent_polarity.documents:  # for loop to search the list for docnames and scan text for polarity
+        sentiment_polarity.append("{:.4f}".format(document.score))  # appending sentiment polarity to new list
+
+    polarity = round(float(sentiment_polarity[0]), 4)
+    return polarity
+
+
+if __name__ == '__main__':
+    arb()
+
