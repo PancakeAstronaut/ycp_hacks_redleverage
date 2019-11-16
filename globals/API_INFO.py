1c5b700b414640f85c0c03e38640a721ef2d10b7
PancakeAstronaut <senormrsir@gmail.com>
2019-11-15 20:33:15 -0500

    added a discord section for discord API stuff

--- globals/API_INFO.py
+++ globals/API_INFO.py
@@ -1,5 +1,7 @@
+# add API connection packages
 from azure.cognitiveservices.language.textanalytics import TextAnalyticsClient      # Azure API imports
 from msrest.authentication import CognitiveServicesCredentials
+import discord
 
 
 # Azure API information for Text analysis
@@ -7,3 +9,5 @@ azure_api_key = "d671436ef9ca42fb9b9205b8b209528c"
 credentials = CognitiveServicesCredentials(azure_api_key)
 azure_endpoint_url = "https://eastus.api.cognitive.microsoft.com/"
 azure_text_analytics = TextAnalyticsClient(endpoint=azure_endpoint_url, credentials=credentials)
+
+# Discord API stuff and globals
