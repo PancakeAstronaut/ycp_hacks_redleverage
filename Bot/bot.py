0f75343044f581aa506708d71d6d90ab9c05a306
viperc46 <viperc46@gmail.com>
2019-11-15 21:55:46 -0500

    inital bot connections

--- Bot/bot.py
+++ Bot/bot.py
@@ -2,7 +2,7 @@ import discord
 
 
 GUILD = "Red Leverage"
-token = 'NjQ1MDcxODQ4ODc0MTgwNjQ5.Xc9QEg.ShPLETHVqkdOVQGlWkEp8N5ZKTs'
+
 
 
 class MyClient(discord.Client):
@@ -13,9 +13,8 @@ class MyClient(discord.Client):
                 print(f'{self.user} is connected to {guild.name}(id: {guild.id})')
 
     async def on_message(self, message):
-        print(f'id:{id.user}')
         print('Message from {0.author}: {0.content}'.format(message))
 
 
 client = MyClient()
-client.run(token)
+client.run('NjQ1MDcxODQ4ODc0MTgwNjQ5.Xc9lAw.7PW6MIVbTrvUGoKsth9-9ovISw8')
